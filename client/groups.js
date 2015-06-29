c1999e56a30c4893f4f348d585b4a955f085a6ec
Max Malm <benjick@dumfan.net>
2015-06-29 22:54:27 +0200

    search gui

--- client/groups.js
+++ client/groups.js
@@ -10,11 +10,24 @@ Template.groups.helpers({
 			return "selected"
 		}
 		//return false
-	}
+	},
+	searchTerm: function() {
+		if(FlowRouter.getRouteName() === 'search') {
+			return FlowRouter.getParam("term")
+		}
+	},
 });
 
 Template.groups.events({
 	'change select': function (event) {
 		FlowRouter.go('/' + event.currentTarget.value)
-	}
+	},
+	'keyup input': function (e) {
+		if(e.target.value.length === 0) {
+			FlowRouter.go('/');
+		}
+		if(e.target.value.length >= 3) {
+			FlowRouter.go('/search/' + e.target.value);
+		}
+	},
 });
