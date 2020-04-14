c5df6a5a38005d15fe85110f3c85a25457582472
Max Malm <benjick@dumfan.net>
2020-04-14 22:14:23 +0200

    Add timeago

--- client/footer.js
+++ client/footer.js
@@ -1,14 +1,15 @@
 import {Template} from 'meteor/templating';
 import {Settings} from '../lib/booze';
+import * as timeago from 'timeago.js';
 
 Template.footer.helpers({
   settings() {
     return Settings.findOne('settings');
   },
-});
-
-Template.footer.helpers({
   equals(a, b) {
     return a === b;
+  },
+  timeAgo(timestamp) {
+    return timeago.format(timestamp);
   },
 });
